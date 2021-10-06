97b2bb7223af0d4569680b627d1bd5733f69ee1a
radical autist <56npyfygybys@gmail.com>
2021-10-06 16:35:38 +0300

    fix crossProduct, add Vector4/Matrix4 constructors, operator overloads

--- src/math.hpp
+++ src/math.hpp
@@ -2,13 +2,24 @@
 #define MATH_HPP
 
 #include <math.h>
+#include <stdint.h>
 #include <immintrin.h>
 
 #define PI 3.14159265359f
 // PI / 180. Used for to convert degrees to radians
 #define PI_BY_180 0.01745329251994329576923690768489f
 
-namespace math {
+// Load values from a simd register into a vector of a given type
+#define GET_RESULT(VectorType, simdRegister) \
+  VectorType result;                         \
+  _mm_store_ps (&result.x, simdRegister);    \
+  return result
+
+namespace rei::math {
+
+[[nodiscard]] constexpr inline float radians (float degrees) noexcept {
+  return degrees * PI_BY_180;
+}
 
 // Set of functions that operate on/return a 128 bit simd register
 namespace simd::m128 {
@@ -55,8 +66,7 @@ namespace simd::m128 {
   columns[1] = _mm_shuffle_ps (b, b, rightMask);
   columns[2] = _mm_shuffle_ps (a, a, rightMask);
   columns[3] = _mm_shuffle_ps (b, b, leftMask);
-
-  return _mm_sub_ps (_mm_mul_ps (columns[0], columns[2]), _mm_mul_ps (columns[1], columns[3]));
+  return _mm_sub_ps (_mm_mul_ps (columns[0], columns[1]), _mm_mul_ps (columns[2], columns[3]));
 }
 
 }
@@ -65,10 +75,10 @@ struct alignas (16) Vector3 {
   float x, y, z;
 
   Vector3 () = default;
-  Vector3 (float x, float y, float z) : x {x}, y {y}, z {z} {}
-  Vector3 (float scalar) : x {scalar}, y {scalar}, z {scalar} {}
+  constexpr Vector3 (float x, float y, float z) : x {x}, y {y}, z {z} {}
+  constexpr Vector3 (float scalar) : x {scalar}, y {scalar}, z {scalar} {}
 
-  inline __m128 load () const noexcept {
+  [[nodiscard]] inline __m128 load () const noexcept {
     return _mm_set_ps (0.f, z, y, x);
   }
 
@@ -105,29 +115,105 @@ struct alignas (16) Vector3 {
   }
 };
 
-struct Vector4 {
-  float x, y, z, w;
-};
-
-struct Matrix4 {
-  Vector4 rows[4];
-};
-
 inline Vector3 operator + (const Vector3& a, const Vector3& b) noexcept {
-  Vector3 result;
-  _mm_store_ps (&result.x, _mm_add_ps (a.load (), b.load ()));
-  return result;
+  GET_RESULT (Vector3, _mm_add_ps (a.load (), b.load ()));
 }
 
 inline Vector3 operator * (const Vector3& a, const Vector3& b) noexcept {
-  Vector3 result;
-  _mm_store_ps (&result.x, _mm_mul_ps (a.load (), b.load ()));
-  return result;
+  GET_RESULT (Vector3, _mm_mul_ps (a.load (), b.load ()));
 }
 
-[[nodiscard]] constexpr inline float radians (float degrees) noexcept {
-  return degrees * PI_BY_180;
+struct Vector4 {
+  float x, y, z, w;
+
+  Vector4 () = default;
+  constexpr Vector4 (float scalar) :
+    x {scalar}, y {scalar}, z {scalar}, w {scalar} {}
+
+  constexpr Vector4 (float x, float y, float z, float w) :
+    x {x}, y {y}, z {z}, w {w} {}
+
+  [[nodiscard]] inline __m128 load () const noexcept {
+    return _mm_load_ps (&this->x);
+  }
+};
+
+inline Vector4 operator + (const Vector4& a, const Vector4& b) noexcept {
+  GET_RESULT (Vector4, _mm_add_ps (a.load (), b.load ()));
 }
+
+inline Vector4 operator * (const Vector4& vector, float scalar) noexcept {
+  GET_RESULT (Vector4, _mm_mul_ps (vector.load (), _mm_set1_ps (scalar)));
+}
+
+struct Matrix4 {
+  Vector4 rows[4];
+
+  Matrix4 () = default;
+  Matrix4 (float value) {
+    rows[0].x = value;
+    rows[0].y = 0.f;
+    rows[0].z = 0.f;
+    rows[0].w = 0.f;
+
+    rows[1].x = 0.f;
+    rows[1].y = value;
+    rows[1].z = 0.f;
+    rows[1].w = 0.f;
+
+    rows[2].x = 0.f;
+    rows[2].y = 0.f;
+    rows[2].z = value;
+    rows[2].w = 0.f;
+
+    rows[3].x = 0.f;
+    rows[3].y = 0.f;
+    rows[3].z = 0.f;
+    rows[3].w = value;
+  }
+
+  Matrix4 (
+    const Vector4& a,
+    const Vector4& b,
+    const Vector4& c,
+    const Vector4& d) {
+
+    rows[0].x = a.x;
+    rows[0].y = a.y;
+    rows[0].z = a.z;
+    rows[0].w = a.w;
+
+    rows[1].x = b.x;
+    rows[1].y = b.y;
+    rows[1].z = b.z;
+    rows[1].w = b.w;
+
+    rows[2].x = c.x;
+    rows[2].y = c.y;
+    rows[2].z = c.z;
+    rows[2].w = c.w;
+
+    rows[3].x = d.x;
+    rows[3].y = d.y;
+    rows[3].z = d.z;
+    rows[3].w = d.w;
+  }
+
+  inline Vector4& operator [] (uint8_t index) noexcept {
+    return rows[index];
+  }
+
+  inline const Vector4& operator [] (uint8_t index) const noexcept {
+    return rows[index];
+  }
+
+  static inline void translate (Matrix4& matrix, const Vector3& vector) noexcept {
+    matrix[3] =
+      matrix[0] * vector.x +
+      matrix[1] * vector.y +
+      matrix[2] * vector.z + matrix[3];
+  }
+};
 
 void lookAt (
   const Vector3& eye,
